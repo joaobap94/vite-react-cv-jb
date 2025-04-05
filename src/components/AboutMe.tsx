cba7b180cd9485665b11a05d2d04806d3190fc40
joaobap94 <joao.batista.wk.@gmail.com>
2025-04-05 16:54:24 +0100

    react-port about me section

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 
 const AboutMe: React.FC = () => {
   return (
-    <section id="about">
-      <h2>About Me</h2>
-      {/* About Me content will go here */}
+    <section id="about" className="my-5">
+      <Container>
+        <Row className="align-items-center">
+          <Col md={6} className="text-start">
+            <h2>Sobre mim</h2>
+            <p className="lead">
+              João Batista, sou de Santarém e nasci em Julho de 1994. Comecei a estudar Informática na Escola Dr. Ginestal Machado, onde frequentei o curso de Programação de Sistemas Informáticos. Depois deste, licenciei-me em Informática, na Escola Superior de Gestão e Tecnologia de Santarém, com 21 anos.
+            </p>
+          </Col>
+          <Col md={6} className="text-center">
+            <Image 
+              src="/img/mainfb.jpg" 
+              alt="João Batista" 
+              roundedCircle
+              fluid
+              style={{ maxWidth: '300px', height: 'auto' }}
+            />
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 };
