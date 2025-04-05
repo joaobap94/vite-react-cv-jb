21c536092d49623df9f48f4b621ac3175467c426
joaobap94 <joao.batista.wk.@gmail.com>
2025-04-05 21:13:36 +0100

    react-port certificates

--- src/components/Certificates/Certificates.tsx
+++ src/components/Certificates/Certificates.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { certificateData, Certificate } from '../../data/certificate-data';
 import './Certificates.scss';
+import { useTranslation } from 'react-i18next';
 
 const Certificates: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <section id="certificates" className="py-5">
       <Container>
-        <h2 className="text-start mb-4">Certificados</h2>
-        <p className="text-start mb-5">Microsoft certified em algumas áreas</p>
+        <h2 className="text-start mb-4">{t('certificates_section.title')}</h2>
+        <p className="text-start mb-5">{t('certificates_section.subtitle')}</p>
         
         <Row xs={1} sm={2} md={3} lg={4} className="g-3 justify-content-center">
           {certificateData.map((cert: Certificate) => (
